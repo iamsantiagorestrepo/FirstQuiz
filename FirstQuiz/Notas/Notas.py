1e0dc61afa5d25a543eaf3b1eeb50a521fdd05c1
iamsantiagorestrepo <santiago.guevararestrepo@gmail.com>
2024-09-09 19:12:52 -0500

    Add files via upload

--- FirstQuiz/Notas/Notas.py
+++ FirstQuiz/Notas/Notas.py
@@ -1,18 +1,29 @@
 #Este programa muestra el promedio del estudiante, su clasificación de rendimiento y el monto final a pagar por la matrícula
 
-nota1 = float(input("Ingresa la primer nota de 1 a 5: "))
+nota1 = float(input("Ingresa la primera nota de 1 a 5: "))
 nota2 = float(input("Ingresa la segunda nota de 1 a 5: "))
-nota3 = float(input("Ingresa la tercer nota de 1 a 5: "))
+nota3 = float(input("Ingresa la tercera nota de 1 a 5: "))
 nota4 = float(input("Ingresa la cuarta nota de 1 a 5: "))
 
 costo_matricula = float(input("Introduce el costo total de la matrícula: "))
 
 promedio = (nota1 + nota2 + nota3 + nota4) / 4
 
-if promedio <= 4:
-    descuento = 0.20
 
-costo_final = costo_matricula * 0.80
+if 4 <= promedio <= 5:
+    calificacion = "Excelente"
+    descuento = 0.20  # 20% de descuento
+elif 3 <= promedio < 4:
+    calificacion = "Bien"
+    descuento = 0.00  # Sin descuento
+else:
+    calificacion = "Deficiente"
+    descuento = 0.00  # Sin descuento
 
-print(f"Promedio del estudiante: {promedio}")
-print(f"Monto final a pagar de matrícula: ${costo_final}")
+
+costo_final = costo_matricula * (1 - descuento)
+
+
+print(f"Promedio del estudiante: {promedio:.2f}")
+print(f"Calificación de rendimiento: {calificacion}")
+print(f"Monto final a pagar por la matrícula: ${costo_final:.2f}")
